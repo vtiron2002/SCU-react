90862d3340d9e1f176fa348d5a739c1d7a277018
kidus yilma <kidusyilma@gmail.com>
2020-08-26 20:57:44 -0700

    added login

--- src/App.js
+++ src/App.js
@@ -1,40 +1,30 @@
 import React, { Fragment } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {
-  CssBaseline,
-  withStyles,
-} from '@material-ui/core';
+import { CssBaseline, withStyles } from '@material-ui/core';
 
-import AppHeader from './components/AppHeader';
-import LogoHeader from './components/LogoHeader';
-import Quotes from './components/Quotes';
-import InfoSection from './components/Info';
-import MemberJoinSection from './components/MemberJoinSection';
 import Home from './pages/Home';
+import Login from './pages/loginPage/loginPage';
 
-const styles = theme => ({
-  main: {
-    padding: theme.spacing(3),
-    [theme.breakpoints.down('xs')]: {
-      padding: theme.spacing(2),
-    },
-  },
+const styles = (theme) => ({
+	main: {
+		padding: theme.spacing(3),
+		[theme.breakpoints.down('xs')]: {
+			padding: theme.spacing(2)
+		}
+	}
 });
 
 const App = ({ classes }) => (
-  <Fragment>
-    <CssBaseline />
-    <AppHeader />
-    <main className={classes.main}>
-      <Home />
-      <Route exact path="/" component={Home} />
-      <LogoHeader />
-      <Quotes />
-      <InfoSection />
-      <MemberJoinSection />
-       </main>
-     </Fragment>
+	<Fragment>
+		<CssBaseline />
+		<main className={classes.main}>
+			<Switch>
+				<Route exact path="/" component={Home} />
+				<Route exact path="/login" component={(props) => <Login {...props} />} />
+			</Switch>
+		</main>
+	</Fragment>
 );
 
 export default withStyles(styles)(App);
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,19 @@
 import React from 'react';
-import {
-  Typography,
-} from '@material-ui/core';
+import AppHeader from '../components/AppHeader';
+import LogoHeader from '../components/LogoHeader';
+import Quotes from '../components/Quotes';
+import InfoSection from '../components/Info';
+import MemberJoinSection from '../components/MemberJoinSection';
+
+import { Typography } from '@material-ui/core';
 
 export default () => (
-  <Typography variant="h4"></Typography>
+	<div>
+		<AppHeader />
+		<Typography variant="h4" />
+		<LogoHeader />
+		<Quotes />
+		<InfoSection />
+		<MemberJoinSection />
+	</div>
 );
