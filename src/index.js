f6d940019d3a66169d89f3f628935d269692761c
Abinas <youbeapro@gmail.com>
2020-08-27 22:55:56 -0400

    Added a news page

--- src/index.js
+++ src/index.js
@@ -3,23 +3,23 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Home from './pages/Home';
-import React, { Component} from 'react'
+import React, { Component } from 'react'
 import './App.css';
 import "./index.css";
 import {
-    BrowserRouter,
-    Switch,
-    Route,
-    Link,
-    useParams,
-  } from "react-router-dom";
-import { Navbar,Nav,NavDropdown,NavItem,NavLink,FormControl,Button } from 'react-bootstrap'
+  BrowserRouter,
+  Switch,
+  Route,
+  Link,
+  useParams,
+} from "react-router-dom";
+import { Navbar, Nav, NavDropdown, NavItem, NavLink, FormControl, Button } from 'react-bootstrap'
 import AppHeader from './components/AppHeader';
 import Contact from './pages/Contact';
 import Founders from './pages/Founders';
 import Mission from './pages/Mission';
 import Training from './pages/Training';
-import News from './components/News';
+import News from './pages/News';
 import Volunteer from './pages/Volunteer';
 import Grants from './pages/Grants';
 import TaskForce from './pages/TaskForce';
@@ -35,16 +35,16 @@ ReactDOM.render(
     <div>
       <AppHeader />
 
-      <Route exact path="/" component={App}/>
-      <Route path="/founders" component={Founders}/>
-      <Route path="/contact" component={Contact}/>
-      <Route path="/mission" component={Mission}/>
-      <Route path="/training" component={Training}/>
-      <Route path="/news" component={News}/>
-      <Route path="/volunteer" component={Volunteer}/>
-      <Route path="/grants" component={Grants}/>
-      <Route path="/taskForce" component={TaskForce}/>
-      <Route path="/events" component={Events}/>
+      <Route exact path="/" component={App} />
+      <Route path="/founders" component={Founders} />
+      <Route path="/contact" component={Contact} />
+      <Route path="/mission" component={Mission} />
+      <Route path="/training" component={Training} />
+      <Route path="/news" component={News} />
+      <Route path="/volunteer" component={Volunteer} />
+      <Route path="/grants" component={Grants} />
+      <Route path="/taskForce" component={TaskForce} />
+      <Route path="/events" component={Events} />
 
     </div>
   </BrowserRouter>,
---
+++ src/pages/News.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import NewsCard from '../components/NewsCard'
+
+const API_KEY = "722c05c77ee94c99bb4d8d5e18dedddc";
+const News = () => {
+    const [state, setState] = useState(null)
+
+    useEffect(() => {
+        const abortController = new AbortController()
+        const signal = abortController.signal;
+
+        let date = new Date();
+        if (date.getDate() === 1) {
+            console.log(true)
+            date = new Date(date.getTime() - 86400000)
+        }
+        const today = date.getFullYear() + "-" + ("0" + (date.getMonth() + 1)).slice(-2) + "-" + ("0" + (date.getDate() - 1)).slice(-2)
+
+        console.log(today)
+        const url = `https://newsapi.org/v2/everything?q=blm&from=${today}&to=${today}&apiKey=${API_KEY}`;
+
+        let result = null;
+        axios.get(url, { signal: signal })
+            .then(res => {
+                setState(res.data.articles)
+            }).catch(err => {
+                console.log(err)
+            })
+
+        return () => { abortController.abort() }
+    }, [])
+
+    return (
+        <div style={{ display: 'flex', flexWrap: 'wrap', alignItems: 'center', justifyContent: 'center' }}>
+            {state ? state.map((article, index) => <NewsCard key={index} title={article.title} urlToImage={article.urlToImage} content={article.content} url={article.url} />) : 'no data yet'}
+        </div>
+    )
+}
+
+export default News
