e187859863ebc1e46142af3a6ba03fbd762a6efb
tyrainman427 <tyraineytech@gmail.com>
2020-09-21 21:39:07 -0400

    added missing files from repo

--- src/App.js
+++ src/App.js
@@ -4,21 +4,12 @@ import { CssBaseline} from '@material-ui/core';
 import { styled } from '@material-ui/core/styles';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-import InfoSection from './components/Info';
-import MemberJoinSection from './components/MemberJoinSection';
-import FollowUs from './components/FollowUs';
-import LogoHeader from './components/LogoHeader'
-import Quotes from './components/Quotes'
+
 
 
 const BaseApp = ({ className }) => (
 	<main className={className}>
-		<CssBaseline />
-		<LogoHeader />
-		<Quotes />
-		<InfoSection />
-		<MemberJoinSection />
-		<FollowUs />
+
 	</main>
 );
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 
 import ReactDOM from 'react-dom';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
 import React from 'react'
 import './App.css';
 import "./index.css";
@@ -51,9 +51,6 @@ ReactDOM.render(
   document.getElementById('root'),
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
 
 if (module.hot) module.hot.accept();
